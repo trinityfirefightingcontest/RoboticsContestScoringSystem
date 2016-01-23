cdc020322cedf096b33611ec00ea6e5834788f28
binay <binay@jana.com>
2016-01-23 18:59:56 -0500

    added gunicorn and flask support

---
+++ constants/settings.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+mysql_host = 'localhost'
+mysql_database = 'challenge'
+mysql_user = 'root'
+mysql_password = 'D5QfPNvVSfq5qE6v0UlngA=='
+mysql_use_sample = True
+mysql_port = 3306
+redis_host = 'localhost'
+redis_port = 6379
+redis_db = 9
---
+++ scoringsystem/scoringapp.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+
+import os
+from datetime import datetime
+import hashlib
+from initialize_registry import load_registry
+load_registry()
+from flask import Flask, request
+from werkzeug.wsgi import SharedDataMiddleware
+from libraries.sentry_client import SentryClient
+
+app = Flask('robotics_scoring_system')
+app.debug = True
+app.secret_key = '9dBrJXZMW8E44yfODtLuxp5f0Yz2cFfPfUe2Cs2n'
+
+APP_NAME = 'robotics_scoring_system'
+
+app.config.update(
+    APP_SESSION_NAME='robotics_scoring_system',
+    SENTRY_DSN='https://858c1ce455dc43538c50f8ca3b3358ce:93608270372b4b939737b04a3c816612@app.getsentry.com/60618'
+)
+SentryClient.init_flask(app)
+
+if app.config.get('ENV') == 'development':
+    cacheStatic = False
+else:
+    cacheStatic = True
+
+app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
+    '/static': os.path.join(os.path.dirname(__file__), 'static'),
+}, cache=cacheStatic)
+
+# blueprints and views
+from blueprints.main import main
+app.register_blueprint(main)
+
+
+def _get_etag():
+    """
+    Set an eTag value for the current page
+    SHA1 should generate well-behaved etags
+    The values that make up the string used for the ETag value are:
+    the script root value
+    the request path
+    the current timestamp
+    """
+    now = datetime.utcnow().isoformat()
+    etag_src = "%s--%s-%s" % (request.script_root, request.path, now,)
+    etag = hashlib.sha1(etag_src.encode('utf8', 'replace')).hexdigest()
+    return etag
+
+
+@app.after_request
+def cache_control(response):
+    max_age = 1
+    cache_control_str = "max-age=%s, private, must-revalidate" % max_age
+    response.headers.add("Cache-Control", cache_control_str)
+    etag = _get_etag()
+    response.set_etag(etag)
+    return response
+
+if __name__ == '__main__':
+    app.run()
