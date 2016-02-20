a57ee23990c798f078760e0a20d07f39ee99318f
Binay Poudel <binaypoudel@Binays-MacBook-Pro.local>
2016-02-20 12:34:53 -0500

    fixed the load.py file

--- load.py
+++ load.py
@@ -1,13 +1,19 @@
 # -*- coding: utf-8 -*-
 from initialize_registry import load_registry
 import registry as r
+import MySQLdb
+from constants import settings
 
 
 def run():
+    db = MySQLdb.connect(
+        host=settings.mysql_host,
+        port=settings.mysql_port,
+        user=settings.mysql_user,
+        passwd=settings.mysql_password)
+    db.query('CREATE DATABASE IF NOT EXISTS scoring_system;')
     load_registry()
-    r.get_registry()['MY_SQL'].query(
-        'CREATE DATABASE IF NOT EXISTS scoring_system;'
-    )
+
     r.get_registry()['MY_SQL'].query(
         'ALTER DATABASE scoring_system CHARACTER SET '
         'utf8 COLLATE utf8_general_ci;'
