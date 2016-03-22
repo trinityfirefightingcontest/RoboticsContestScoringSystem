e28ec47d0632aa7a108408647022d8dc499388b9
= <binay@jana.com>
2016-03-21 21:43:28 -0400

    some refactors

---
+++ libraries/utilities/run_parameters.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8; -*-
+
+
+class RunParameters(object):
+    NAME = 'name'
+    RUN_DISQUALIFIED = 'run_disqualified'
+    SECONDS_JUDGE_1 = 'seconds_to_put_out_candle_1'
+    SECONDS_JUDGE_2 = 'seconds_to_put_out_candle_2'
+    NON_AIR = 'non_air'
+    FURNITURE = 'furniture'
+    ARBITRARY_START = 'arbitrary_start'
+    RETURN_TRIP = 'return_trip'
+    NO_CANDLE_CIRCLE = 'no_candle_circle'
+    STOPPED_WITHIN_30 = 'stopped_within_30'
+    CANDLE_DETECTED = 'candle_detected'
+    NUM_ROOMS_SEARCHED = 'number_of_rooms_searched'
+    KICKED_DOG = 'kicked_dog'
+    TOUCHED_CANDLE = 'touched_candle'
+    WALL_CONTACT_CMS = 'wall_contact_cms'
+    RAMP_USED = 'ramp_used'
+    BABY_RELOCATED = 'baby_relocated'
+    ALL_CANDLES = 'all_candles'
+    VERSA_VALVE_USED = 'versa_valve_used'
+
+    # name of all html form inputs, order matters
+    ALL = [
+        NAME,
+        RUN_DISQUALIFIED,
+        SECONDS_JUDGE_1,
+        SECONDS_JUDGE_2,
+        NON_AIR,
+        FURNITURE,
+        ARBITRARY_START,
+        RETURN_TRIP,
+        NO_CANDLE_CIRCLE,
+        STOPPED_WITHIN_30,
+        CANDLE_DETECTED,
+        NUM_ROOMS_SEARCHED,
+        KICKED_DOG,
+        TOUCHED_CANDLE,
+        WALL_CONTACT_CMS,
+        RAMP_USED,
+        BABY_RELOCATED,
+        ALL_CANDLES,
+        VERSA_VALVE_USED
+    ]
+
+    BOOLEANS = [
+        RUN_DISQUALIFIED,
+        NON_AIR,
+        FURNITURE,
+        ARBITRARY_START,
+        RETURN_TRIP,
+        NO_CANDLE_CIRCLE,
+        STOPPED_WITHIN_30,
+        CANDLE_DETECTED,
+        KICKED_DOG,
+        TOUCHED_CANDLE,
+        RAMP_USED,
+        BABY_RELOCATED,
+        ALL_CANDLES,
+        VERSA_VALVE_USED
+    ]
+
+    STRING_PARAMS = [
+        NAME,
+        SECONDS_JUDGE_1,
+        SECONDS_JUDGE_2,
+        NUM_ROOMS_SEARCHED,
+        WALL_CONTACT_CMS
+    ]
---
+++ libraries/utilities/score_calculator.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8; -*-
+
+
+class ScoreCalculator(object):
+    @staticmethod
+    def get_best_scores(runs):
+        l1_scores = [run['score'] for run in runs if run['level'] == 1]
+        l2_scores = [run['score'] for run in runs if run['level'] == 2]
+        l3_scores = [run['score'] for run in runs if run['level'] == 3]
+
+        best_scores = {
+            'LS1': min(l1_scores) if l1_scores else 600,
+            'LS2': min(l2_scores) if l2_scores else 600,
+            'LS3': min(l3_scores) if l3_scores else 600
+        }
+        attempted_levels = set([run['level'] for run in runs])
+        return best_scores, attempted_levels, sum(best_scores.val())
